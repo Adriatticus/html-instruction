3be90abd634b421f477c44945082194a6eec505c
Adriatticus <4CorderJ@gmail.com>
2025-03-26 08:38:08 -0400

    ready

---
+++ 55244Labs/55244Labs/LabFiles/JavaScript/Module 4/Lab2/HomePageAutomation.js
@@ -0,0 +1,31 @@
+var styletext="\n\
+@keyframes flyaway {  \n\
+0% {  \n\
+    transition: 5s ease-in;  \n\
+  }  \n\
+  100% {  \n\
+    transform: translate(350px, 100px); \n\
+  }  \n\
+}  \n\
+\n\
+#airplane1 {  \n\
+  animation: flyaway 10s ease-in forwards;  \n\
+  -moz-animation-iteration-count: 1;  \n\
+  animation-iteration-count: 1;   \n\
+}  \n\
+"
+var newStyleBlock = document.createElement("style");
+newStyleBlock.textContent = styletext;
+document.head.appendChild(newStyleBlock);
+var airplane = document.getElementById("airplane1");
+airplane.addEventListener("animationend",
+  function(){airplane.style.display="none"}, false);
+
+   /*
+ Author: Mike and Me
+ Date: 3/26/25
+ Used by: default.html
+ Notes:
+ Creates a new <style> block to slowly move the image off of the page.
+ Add an event handler to hide the image after the animation.
+ */
--- 55244Labs/55244Labs/LabFiles/JavaScript/Module 4/Lab2/MenuPopups.js
+++ 55244Labs/55244Labs/LabFiles/JavaScript/Module 4/Lab2/MenuPopups.js
@@ -2,7 +2,7 @@ var popdiv = document.createElement("div");
 popdiv.id = "menumessage";
 popdiv.textContent = "default text for testing";
 popdiv.style.color = "orange";
-popdiv.style.border = "1pc solid white";
+popdiv.style.border = "1px solid white";
 popdiv.style.borderRadius = "10px";
 popdiv.style.backgroundColor = "black";
 popdiv.style.position = "absolute";
@@ -14,14 +14,40 @@ var tabs = document.querySelectorAll("#topNav a");
 for (var index = 0; index < tabs.length; index++) {
   var tab = tabs[index];
   tab.onmouseenter = function () {
-    SowPopup(this);
+    ShowPopup(this);
   };
   tab.onmouseleave = function () {
     HidePopup();
   };
 }
-
-var popdiv = document.getElementById("menumessage");
-popdiv.style.left = "30px";
-popdiv.style.top = "110px";
-popdiv.style.display = "block";
+function ShowPopup(tab) {
+  var popdiv =document.getElementById("menumessage");
+  popdiv.style.left = tab.offsetLeft + "px";
+  popdiv.style.top = (tab.offsetTop +33) + "px";
+  //popdiv.style.left = "30px";
+  //popdiv.style.top = "110px";
+  //popdiv.style.display = "block";
+  var popuptext = "";
+  switch (tab.textContent) {
+    case "Home":
+      popuptext = "Return to the home page.";
+      break;
+    case "Toys":
+      popuptext = "See our latest toys, including boats, planes, and cars!";
+      break;
+    case "Sports":
+      popuptext = "See our new line of sporting goods!"
+      break;
+    default:
+      popuptext = "";
+      break;
+  }
+  popdiv.innerHTML = popuptext;
+  if (popuptext != "") { //(popuptext != "") ? popdiv.innterHTML = popuptext: popdiv.style.display = "block"; -- the real KING = popdiv.style.display = (popuptext != "") ?"block": "none";
+    popdiv.style.display = "block";
+  }
+}
+function HidePopup() {
+  var popdiv = document.getElementById("menumessage");
+  popdiv.style.display = "none";
+}
